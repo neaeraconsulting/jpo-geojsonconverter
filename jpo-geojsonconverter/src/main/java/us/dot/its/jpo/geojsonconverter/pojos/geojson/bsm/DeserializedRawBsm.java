9d2e9d1f590f013b0851e79bcd76211a9114aab1
Drew Johnston <drewjj901@gmail.com>
2025-07-23 14:39:41 -0600

    POJO annotations for BSM and ConnectingLanes

--- jpo-geojsonconverter/src/main/java/us/dot/its/jpo/geojsonconverter/pojos/geojson/bsm/DeserializedRawBsm.java
+++ jpo-geojsonconverter/src/main/java/us/dot/its/jpo/geojsonconverter/pojos/geojson/bsm/DeserializedRawBsm.java
@@ -1,25 +1,23 @@
 package us.dot.its.jpo.geojsonconverter.pojos.geojson.bsm;
 
-import us.dot.its.jpo.geojsonconverter.validator.JsonValidatorResult;
-import us.dot.its.jpo.ode.model.OdeMessageFrameData;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import us.dot.its.jpo.geojsonconverter.validator.JsonValidatorResult;
+import us.dot.its.jpo.ode.model.OdeMessageFrameData;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Slf4j
 public class DeserializedRawBsm {
-
     OdeMessageFrameData odeBsmMessageFrameData;
     JsonValidatorResult validatorResults;
     boolean validationFailure = false;
     String failedMessage = null;
-
-    @Override
-    public String toString() {
-        return "{" + " odeBsmMessageFrameData='" + getOdeBsmMessageFrameData() + "'" + ", validatorResults='"
-                + getValidatorResults() + "'" + ", validationFailure='" + isValidationFailure() + "'"
-                + ", failedMessage='" + getFailedMessage() + "'" + "}";
-    }
 }
--- jpo-geojsonconverter/src/main/java/us/dot/its/jpo/geojsonconverter/pojos/geojson/bsm/ProcessedBsm.java
+++ jpo-geojsonconverter/src/main/java/us/dot/its/jpo/geojsonconverter/pojos/geojson/bsm/ProcessedBsm.java
@@ -1,15 +1,19 @@
 package us.dot.its.jpo.geojsonconverter.pojos.geojson.bsm;
 
-import com.fasterxml.jackson.annotation.*;
-
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.extern.slf4j.Slf4j;
 import us.dot.its.jpo.geojsonconverter.pojos.geojson.BaseFeature;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Slf4j
 public class ProcessedBsm<TGeometry> extends BaseFeature<Integer, TGeometry, BsmProperties> {
     @JsonCreator
     public ProcessedBsm(@JsonProperty("id") Integer id, @JsonProperty("geometry") TGeometry geometry,
             @JsonProperty("properties") BsmProperties properties) {
         super(id, geometry, properties);
     }
-
-
 }
