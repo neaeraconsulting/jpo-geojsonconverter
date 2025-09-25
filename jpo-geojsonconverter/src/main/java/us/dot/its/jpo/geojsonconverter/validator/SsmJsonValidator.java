8e5e4eb814f34eb48c864747b976b86ee3ecf111
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-09-25 11:50:27 -0600

    Use virtual threads for shutdown hooks

--- jpo-geojsonconverter/src/main/java/us/dot/its/jpo/geojsonconverter/validator/SsmJsonValidator.java
+++ jpo-geojsonconverter/src/main/java/us/dot/its/jpo/geojsonconverter/validator/SsmJsonValidator.java
@@ -2,10 +2,12 @@ package us.dot.its.jpo.geojsonconverter.validator;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
 
 /**
  * JSON validator for SSM Messages.
  */
+@Service
 public class SsmJsonValidator extends AbstractJsonValidator {
     /**
      * @param jsonSchemaResource The json schema file in resources/schemas.
