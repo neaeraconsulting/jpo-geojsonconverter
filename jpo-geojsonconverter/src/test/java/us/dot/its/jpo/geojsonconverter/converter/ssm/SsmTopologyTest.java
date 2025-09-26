06ca67e3a52501782cd34af8d51c25ab82032ada
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-09-25 20:04:28 -0600

    ssm topology test

--- jpo-geojsonconverter/src/test/java/us/dot/its/jpo/geojsonconverter/converter/ssm/SsmTopologyTest.java
+++ jpo-geojsonconverter/src/test/java/us/dot/its/jpo/geojsonconverter/converter/ssm/SsmTopologyTest.java
@@ -1,4 +1,97 @@
 package us.dot.its.jpo.geojsonconverter.converter.ssm;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.VoidSerializer;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import us.dot.its.jpo.geojsonconverter.converter.srm.SrmConverter;
+import us.dot.its.jpo.geojsonconverter.converter.srm.SrmTopology;
+import us.dot.its.jpo.geojsonconverter.partitioner.RsuVehicleIdKey;
+import us.dot.its.jpo.geojsonconverter.pojos.geojson.Point;
+import us.dot.its.jpo.geojsonconverter.pojos.geojson.srm.ProcessedSrm;
+import us.dot.its.jpo.geojsonconverter.pojos.geojson.srm.SrmProperties;
+import us.dot.its.jpo.geojsonconverter.pojos.ssm.ProcessedSsm;
+import us.dot.its.jpo.geojsonconverter.serialization.deserializers.JsonDeserializer;
+import us.dot.its.jpo.geojsonconverter.validator.SrmJsonValidator;
+import us.dot.its.jpo.geojsonconverter.validator.SsmJsonValidator;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@Slf4j
+@RunWith(Parameterized.class)
 public class SsmTopologyTest {
+    final String inputTopicName = "topic.OdeSsmJson";
+    final String outputTopicName = "topic.ProcessedSsm";
+
+    @Parameter(0)
+    public String inputJson;
+
+    @Parameters
+    public static Collection<Object[]> params() throws IOException {
+        return Arrays.asList(new Object[][] {
+                { loadResource("json/valid.ssm.json") }
+        });
+    }
+
+    @Test
+    public void topologyTest() {
+        Resource jsonSchemaResource = getResource("schemas/ssm.schema.json");
+        SsmJsonValidator validator = new SsmJsonValidator(jsonSchemaResource);
+        SsmConverter converter = new SsmConverter();
+        Topology topology = SsmTopology.build(inputTopicName, outputTopicName, validator, converter);
+        try (var driver = new TopologyTestDriver(topology)) {
+            var inputTopic = driver.createInputTopic(inputTopicName, new VoidSerializer(), new StringSerializer());
+            var outputTopic = driver.createOutputTopic(outputTopicName,
+                    new JsonDeserializer<>(RsuVehicleIdKey.class), new JsonDeserializer<>(ProcessedSsm.class));
+
+            inputTopic.pipeInput(inputJson);
+
+            List<KeyValue<RsuVehicleIdKey, ProcessedSsm>> results = outputTopic.readKeyValuesToList();
+
+            assertThat(results, hasSize(1));
+
+            KeyValue<RsuVehicleIdKey, ProcessedSsm> result = results.getFirst();
+            RsuVehicleIdKey key = result.key;
+            assertThat(key, notNullValue());
+            assertThat(key.getRsuId(), equalTo("172.18.0.1"));
+            ProcessedSsm processedSsm = result.value;
+            assertThat(processedSsm, notNullValue());
+            assertThat(processedSsm.getAsn1(), notNullValue());
+            assertThat(processedSsm.getOdeReceivedAt(), notNullValue());
+            assertThat(processedSsm.getMessageType(), equalTo("SSM"));
+            assertThat(processedSsm.getStatusList(), hasSize(greaterThan(0)));
+        }
+    }
+
+    private static String loadResource(String path) throws IOException {
+        Resource resource = getResource(path);
+        return readResource(resource);
+    }
+
+    private static Resource getResource(String path) {
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        return resourceLoader.getResource("classpath:" + path);
+    }
+
+    private static String readResource(Resource resource) throws IOException {
+        byte[] bytes = resource.getInputStream().readAllBytes();
+        return  new String(bytes, StandardCharsets.UTF_8);
+    }
 }
