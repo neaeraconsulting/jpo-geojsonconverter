326490b18ce9293dfe00612452884da9fbd6bb4c
john-wiens <jwiens3141@gmail.com>
2025-10-17 11:16:37 -0600

    Adding transformer serialization parameters

--- jpo-geojsonconverter/src/test/java/us/dot/its/jpo/geojsonconverter/converter/ssm/SsmSerializationTest.java
+++ jpo-geojsonconverter/src/test/java/us/dot/its/jpo/geojsonconverter/converter/ssm/SsmSerializationTest.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import us.dot.its.jpo.asn.j2735.r2024.SignalStatusMessage.SignalStatusMessageMessageFrame;
 import us.dot.its.jpo.geojsonconverter.pojos.ssm.ProcessedSsm;
+import us.dot.its.jpo.geojsonconverter.utils.ProcessedSchemaVersions;
 import static us.dot.its.jpo.geojsonconverter.TestResourceUtil.loadResource;
 
 @Slf4j
@@ -23,7 +24,15 @@ public class SsmSerializationTest {
         SignalStatusMessageMessageFrame messageFrame = mapper.readValue(ssmJson, SignalStatusMessageMessageFrame.class);
         ProcessedSsm processedSsm = ssmConverter.processSsm(messageFrame);
 
+
+        System.out.println("\n\n\nSerializing SSM Json" + ssmJson);
+        System.out.println("\n\n\nSerializing Processed SSM" + processedSsm.toString());
+
+
         processedSsm.setOdeReceivedAt(ZonedDateTime.parse("2025-09-18T06:29:28Z"));
+        processedSsm.setAsn1("001E1865B80579181E00F0BD480C95E46CC2981428200408430AA0");
+        processedSsm.setSchemaVersion(ProcessedSchemaVersions.PROCESSED_SSM_SCHEMA_VERSION);
+        processedSsm.setOriginIp("172.18.0.1");
 
         // Forcibly remove newlines and spaces to make sure IDE json formatting doesn't mess up test performance
         String compareString = processedSsm.toString().replaceAll("\n", "").replaceAll(" ", "");
