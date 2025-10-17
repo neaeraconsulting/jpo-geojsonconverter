26ace9652313570897d41d9b23e9ad83a313ed5f
john-wiens <jwiens3141@gmail.com>
2025-10-17 10:36:08 -0600

    Adding Json Serialization for ProcessedSsm

---
+++ jpo-geojsonconverter/src/test/java/us/dot/its/jpo/geojsonconverter/converter/ssm/SsmSerializationTest.java
@@ -0,0 +1,33 @@
+package us.dot.its.jpo.geojsonconverter.converter.ssm;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import org.junit.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import us.dot.its.jpo.asn.j2735.r2024.SignalStatusMessage.SignalStatusMessageMessageFrame;
+import us.dot.its.jpo.geojsonconverter.pojos.ssm.ProcessedSsm;
+import static us.dot.its.jpo.geojsonconverter.TestResourceUtil.loadResource;
+
+@Slf4j
+public class SsmSerializationTest {
+
+    private final static ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void testProcessSsmSerialization() throws IOException {
+        final String ssmJson = loadResource("classpath:json/ssm.message-frame.json");
+        final String referenceProcessedSsmJson = loadResource("classpath:json/sample.processed-ssm.json");
+        SsmConverter ssmConverter = new SsmConverter();
+        SignalStatusMessageMessageFrame messageFrame = mapper.readValue(ssmJson, SignalStatusMessageMessageFrame.class);
+        ProcessedSsm processedSsm = ssmConverter.processSsm(messageFrame);
+
+        processedSsm.setOdeReceivedAt(ZonedDateTime.parse("2025-09-18T06:29:28Z"));
+
+        // Forcibly remove newlines and spaces to make sure IDE json formatting doesn't mess up test performance
+        String compareString = processedSsm.toString().replaceAll("\n", "").replaceAll(" ", "");
+        String compareReference = referenceProcessedSsmJson.replaceAll("\n", "").replaceAll(" ", "");
+        assertEquals(compareReference, compareString);
+    }
+}
