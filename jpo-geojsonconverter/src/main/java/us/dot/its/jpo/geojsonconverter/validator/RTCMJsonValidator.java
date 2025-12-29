b8c23096dd7c7f648ef903d5337b72ccf62ead6f
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-12-28 19:29:14 -0700

    remove unused import

--- jpo-geojsonconverter/src/main/java/us/dot/its/jpo/geojsonconverter/validator/RTCMJsonValidator.java
+++ jpo-geojsonconverter/src/main/java/us/dot/its/jpo/geojsonconverter/validator/RTCMJsonValidator.java
@@ -1,7 +1,5 @@
 package us.dot.its.jpo.geojsonconverter.validator;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 /**
