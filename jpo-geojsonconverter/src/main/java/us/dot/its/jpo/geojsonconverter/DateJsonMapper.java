e25f163e7c3e2c3d4b4ccda975c032357573b22f
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2026-03-22 12:58:17 -0600

    Use Jackson Blackbird module in the singleton object mapper

--- jpo-geojsonconverter/src/main/java/us/dot/its/jpo/geojsonconverter/DateJsonMapper.java
+++ jpo-geojsonconverter/src/main/java/us/dot/its/jpo/geojsonconverter/DateJsonMapper.java
@@ -1,21 +1,26 @@
 package us.dot.its.jpo.geojsonconverter;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.blackbird.BlackbirdModule;
 
 public class DateJsonMapper {
     final static ObjectMapper objectMapper;
 
     static {
         // Initialize the ObjectMapper for general use
-        objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        objectMapper.setSerializationInclusion(Include.NON_NULL);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper = JsonMapper.builder()
+                .addModule(new BlackbirdModule())
+                .addModule(new JavaTimeModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .build();
 
     }
 
